e03d9959bed8e03ddb236bee1dab540f8831d90a
Hussam Hasi <hussamhasi@gmail.com>
2020-04-23 02:01:29 +0300

    project's first stop.

---
+++ src/com/company/UniversityCourse.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UniversityCourse {
+    private List<String> students =new ArrayList<>();
+    private String course;
+    private String instructor;
+
+    UniversityCourse(String courseName,String instructorName){
+        this.course=courseName;
+        this.instructor=instructorName;
+    }
+}
